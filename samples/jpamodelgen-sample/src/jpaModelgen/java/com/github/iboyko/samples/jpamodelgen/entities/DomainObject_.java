6c1fffc179bedec9ffc58df4f1194dde72c3cccf
Illya Boyko <illya.boyko@gmail.com>
2015-04-07 07:42:52 +0200

    sample project added

---
+++ samples/jpamodelgen-sample/src/jpaModelgen/java/com/github/iboyko/samples/jpamodelgen/entities/DomainObject_.java
@@ -0,0 +1,12 @@
+package com.github.iboyko.samples.jpamodelgen.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(DomainObject.class)
+public abstract class DomainObject_ {
+
+
+}
+
