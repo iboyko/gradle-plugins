cc6ebdb267c98328c43ddb9b93a8f1ea0caa44b8
Illya Graf-Boyko <illya.boyko@gmail.com>
2017-11-02 21:59:20 +0100

    new sample objects

--- samples/jpamodelgen-sample/src/main/java/com/github/iboyko/samples/jpamodelgen/entity/DomainObject2.java
+++ samples/jpamodelgen-sample/src/main/java/com/github/iboyko/samples/jpamodelgen/entity/DomainObject2.java
@@ -1,8 +1,22 @@
 package com.github.iboyko.samples.jpamodelgen.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 @Entity
 public class DomainObject2 {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  public Long getId(){
+    return id;
+  }
+
+  public void setId(Long id){
+    this.id = id;
+  }
 
 }
